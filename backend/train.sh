372eabcc31cbb49611a5e28e32103b415be16f03
kimkihoon0515 <tega1996@naver.com>
2022-11-19 21:08:00 +0900

    develop branch added

--- backend/train.sh
+++ backend/train.sh
@@ -1,4 +1,4 @@
-python mlflow_practice.py
+python mlflow_practice.py --epochs 20 --optimizer AdamW --lr 0.0001
 # --epochs : int
 # --batch-size : int
 # --optimizer : str ['SGD','Adam','AdamW'] 중 택1
